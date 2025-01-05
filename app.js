da5c6880e3283f432a2d2c32f5d9fb29470df211
陳玟宇 <wc8183182@gmail.com>
2025-01-05 22:42:22 +0800

    讓資料可以更新

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ saveButton.addEventListener("click", function () {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   console.log(name, email);
-  // alert(`資料已更新:\n姓名: ${name}\n信箱: ${email}`);
 
   document.getElementById("showname").textContent = name;
   document.getElementById("showemail").textContent = email;
